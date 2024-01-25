adde08792f944608ad378b17fc8ddbde5786056c
Shivani40392 <119109896+Shivani40392@users.noreply.github.com>
2024-01-26 01:30:56 +0530

    shivani

--- src/Pages/Humans/Humans.js
+++ src/Pages/Humans/Humans.js
@@ -1,10 +1,58 @@
 import React from 'react'
 import Navbar from '../../Components/Header/Navbar'
-
+import "./Humans.css"
+import speaker from "../../i/saksham.jpeg"
+import { useState } from 'react'
+import youtube from "../../i/youtube.png"
+import sponser from "../../i/sponsor.png"
 const Humans = () => {
+  const [selectedTab, setSelectedTab] = useState('partner');
+  var partner=[speaker,speaker,speaker,speaker,speaker,speaker,speaker,speaker,speaker]
+  var mentor=[youtube,youtube,youtube,youtube]
+  var organise=[sponser,sponser,sponser]
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
+ 
+  const renderImages = () => {
+    if (selectedTab === 'partner') {
+      return partner.map((item, i) => (
+        <div key={i} className='speaker-img1' >
+          <img src={item} alt='' className='insideimg1'/>
+        </div>
+      ));
+    } else if (selectedTab === 'mentor') {
+      return mentor.map((item, i) => (
+        <div key={i} className='speaker-img1'>
+          <img src={item} alt='' className='insideimg1' />
+        </div>
+      ));
+    } else if (selectedTab === 'organizer') {
+      return organise.map((item, i) => (
+        <div key={i} className='speaker-img1'>
+          <img src={item} alt='' className='insideimg1'/>
+        </div>
+      ));
+    }
+  };
+
   return (
     <div>
-        <Navbar/>
+        <div class='maincontainer'>
+          <div className='heading'>Humans</div>
+        
+           <div class='base-button'>
+            <div   className={`partner ${selectedTab === 'partner' ? 'clicked' : ''}`}
+          onClick={() => handleTabClick('partner')}>Partner Reps</div>
+            <div  className={`mentor ${selectedTab === 'mentor' ? 'clicked' : ''}`}
+          onClick={() => handleTabClick('mentor')}>Mentors & Judges</div>
+             <div   className={`orgrainser ${selectedTab === 'organizer' ? 'clicked' : ''}`}
+          onClick={() => handleTabClick('organizer')}>Organizers</div>
+           </div>
+       <div  className='box1'>
+        {renderImages()}
+        </div> 
+        </div>
     </div>
   )
 }
--- src/Pages/Schedule/Schedule.js
+++ src/Pages/Schedule/Schedule.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import Navbar from '../../Components/Header/Navbar'
 import "./Schedule.css"
-
-
+import speaker from "../../i/saksham.jpeg"
+import youtube from "../../i/youtube.png"
 function Schedule() {
+  var data=[{id:1,eventname:'Getting started with Microservices Architecture & Conductor',speaker:'Saksham Solanki',date:'Jan 25, 2024',time:'17:00 IST'},
+  {id:1,eventname:'Getting started with Microservices Architecture & Conductor',speaker:'Saksham Solanki',date:'Jan 25, 2024',time:'17:00 IST'},
+  {id:1,eventname:'Getting started with Microservices Architecture & Conductor',speaker:'Saksham Solanki',date:'Jan 25, 2024',time:'17:00 IST'},
+  {id:1,eventname:'Getting started with Microservices Architecture & Conductor',speaker:'Saksham Solanki',date:'Jan 25, 2024',time:'17:00 IST'},
+  {id:1,eventname:'Getting started with Microservices Architecture & Conductor',speaker:'Saksham Solanki',date:'Jan 25, 2024',time:'17:00 IST'}]
+
+  function playImages(){
+    return data.map((item,i)=>{
+      return(
+          <div className='subbox  '>
+           <div className='boxtext'>
+            {item.eventname}
+            </div>
+            <div className='name'>
+              {item.speaker}
+            </div>
+            <div className='time'>
+              {item.date} . {item.time}
+            </div>
+            <div className='img-button'>
+              <div className='button'>
+              <div className='button-text'>Join</div>
+              </div>
+              
+            <div className='speaker-img'><img src={speaker} alt='' className='insideimg' />
+            </div> 
+            </div>
+            
+          </div>
+      )
+    })
+  }
   return (
     <div>
       
-       <Navbar/>
+      
        <div className="middle-container-Schedule">
-       
+       <div className="heading">Speaker Schedule</div>
+       <div  className='box'>
+        {playImages()}
+        </div>
+        
        </div>
 
     </div>
