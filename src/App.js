ead636d6b82cb1388da4280ab86d0a6dd3961d8f
Manas0066 <manaschaudhary1602@gmail.com>
2024-01-25 23:23:32 +0530

    manas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Home from './Pages/Home/Home';
-import Faq from "./Pages/FAQ/Faq"
+import Faq from "./Pages/FAQ/f4"
 import Schedule from './Pages/Schedule/Schedule';
 import About from './Pages/AboutUs/About';
+import Login from "../src/Pages/Login/login"
 
 
 function App() {
   return (
     <>
-    {/* <Faq/> */}
-    {/* <Home/> */}
-    {/* <Schedule/> */}
+    <Faq/>
+    {/* <Home/>
+    <Schedule/>
     <About/>
+    <Login/> */}
     </>
   );
 }
--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,27 +1,46 @@
-import React from 'react'
-import "./Navbar.css"
-import Img from "../../i/celsius white with coloured.png"
+import React from 'react';
+import "./Navbar.css";
+import CelsiusLogo from "../../i/celsius white with coloured.png";
+// import { GiHamburgerMenu } from "react-icons/gi";
+
 function Navbar() {
   return (
-    <div>
-      <nav className='nav-head'>
-        <ul className='nav-class'>
-            <div className='LOGO'><img src={Img} alt="" /></div>
-            <div className="list-item-nav">
-            <li>Home <div className="under"></div></li>
-            <li>Partners <div className="under"></div></li>
-            <li>Humans <div className="under"></div></li>
-            <li>Schedule <div className="under"></div></li>
-            <li>About Us <div className="under"></div></li>
-            <li>FAQ <div className="under"></div></li>
-            <li>Celsius2k24 <div className="under"></div></li>
-            
-            </div>
-            
-        </ul>
+    <>
+      <nav className='main-nav'>
+
+        {/*1st logo part*/}
+
+        <div className="logo"><img src={CelsiusLogo} alt="Celsius Logo" /></div>
+        
+        {/*2nd menu part*/}
+        
+        <div className="menu-link">
+          <ul>
+            <li>Home</li>
+            <li>Schedule</li>
+            <li>Humans</li>
+            <li>FAQ</li>
+            <li>AboutUs</li>
+            <li>Celsius2k24</li>
+          </ul>
+        </div>
+        
+        {/*social media links*/}
+         
+        <div className="social-media">
+          <ul className="social-media-desktop">
+            <li><a href="">I</a></li>
+            <li><a href="">F</a></li>
+            <li><a href="">Y</a></li>
+          </ul>
+
+          {/* <div className="hamburger-menu"><GiHamburgerMenu/></div> */}
+        </div> 
       </nav>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
